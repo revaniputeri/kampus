147274c800dacc273a73b0ea32707739d2d71347
revaniputeri <revaninandaputeri@gmail.com>
2024-02-23 13:44:34 +0700

    update disc

--- smt2/Algoritma Struktur Dasar/Praktek/Pertemuan 2/src/Buku24.java
+++ smt2/Algoritma Struktur Dasar/Praktek/Pertemuan 2/src/Buku24.java
@@ -1,6 +1,6 @@
 public class Buku24 {
     String judul, pengarang;
-    int halaman, stok, harga;
+    int halaman, stok, harga, totalHarga, hargaBayar;
 
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
@@ -24,5 +24,44 @@ public class Buku24 {
 
     void gantiHarga(int hrg) {
         harga = hrg;
+    }
+
+    public Buku24() {
+
+    }
+
+    public Buku24(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+
+    }
+
+    void hitungHargaTotal(int jml) {
+        totalHarga = harga * jml;
+    }
+
+    double diskon = 0;
+
+    void hitungDiskon() {
+        if (totalHarga > 150000) {
+            diskon = totalHarga * 0.12;
+        } else if (totalHarga > 75_000 && totalHarga < 150_000) {
+            diskon = totalHarga * 0.05;
+        } else {
+            diskon = 0;
+        }
+    }
+
+    void hitungHargaBayar() {
+        hargaBayar = totalHarga - (int) diskon;
+    }
+
+    void tampilTransaksi(){
+        System.out.println("Total Harga: "+totalHarga);
+        System.out.println("Total Diskon: "+(int)diskon);
+        System.out.println("Total Bayar: "+hargaBayar);
     }
 }
--- smt2/Algoritma Struktur Dasar/Praktek/Pertemuan 2/src/BukuMain24.java
+++ smt2/Algoritma Struktur Dasar/Praktek/Pertemuan 2/src/BukuMain24.java
@@ -11,5 +11,19 @@ public class BukuMain24 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        Buku24 bk2 = new Buku24("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+
+        Buku24 BukuReva = new Buku24("It Ends With Us", "Collen Hoover", 300, 10, 230000);
+        BukuReva.tampilInformasi();
+        BukuReva.terjual(8);
+        BukuReva.restock(3);
+        BukuReva.gantiHarga(100_000);
+        BukuReva.hitungHargaTotal(8);
+        BukuReva.hitungDiskon();
+        BukuReva.hitungHargaBayar();
+        BukuReva.tampilTransaksi();  
     }
 }
