5739d3499e2f050c9926b4dff1ae8f7d0d496210
revaniputeri <revaninandaputeri@gmail.com>
2024-02-29 14:24:25 +0700

    new :frog: : percobaan 1

---
+++ smt2/Algoritma Struktur Dasar/Praktek/Pertemuan 3/src/PersegiPanjangDemo.java
@@ -0,0 +1,17 @@
+public class PersegiPanjangDemo {
+    public static void main(String[] args) {
+        PersegiPanjang[] arrayOfPersegiPanjang = new PersegiPanjang[3];
+
+        arrayOfPersegiPanjang[0] = new PersegiPanjang();
+        arrayOfPersegiPanjang[0].panjang = 110;
+        arrayOfPersegiPanjang[0].lebar = 30;
+
+        arrayOfPersegiPanjang[1] = new PersegiPanjang();
+        arrayOfPersegiPanjang[1].panjang = 80;
+        arrayOfPersegiPanjang[1].lebar = 40;
+
+        arrayOfPersegiPanjang[2] = new PersegiPanjang();
+        arrayOfPersegiPanjang[2].panjang = 100;
+        arrayOfPersegiPanjang[2].lebar = 20;
+    }
+}
